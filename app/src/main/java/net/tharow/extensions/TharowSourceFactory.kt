a138fc0f75765b3190d22e7e5101ae5323c4e3ae
Theray Tharow <tharowt@tharow.net>
2026-09-24 19:25:04 -0400

    First Setup Commit

---
+++ app/src/main/java/net/tharow/extensions/TharowSourceFactory.kt
@@ -0,0 +1,5 @@
+package net.tharow.extensions
+
+class TharowSourceFactory {
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven(url = "https://www.jitpack.io")
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven(url = "https://www.jitpack.io")
+    }
+}
+
+rootProject.name = "Tharow Extensions"
+include(":app")
